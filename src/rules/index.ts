2b37241f88109a55f0457404cb4a01ae77e8fc05
iyegoroff <iegoroff@gmail.com>
2023-09-05 02:22:24 +0300

    copy no-floating-promises

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,5 +1,7 @@
 import * as noMisusedRailways from './no-misused-railways'
+import * as noFloatingRailways from './no-floating-railways'
 
 export const rules = {
   [noMisusedRailways.name]: noMisusedRailways.rule,
+  [noFloatingRailways.name]: noFloatingRailways.rule,
 }
--- src/util/index.ts
+++ src/util/index.ts
@@ -3,6 +3,7 @@ import { ESLintUtils } from '@typescript-eslint/utils'
 export * from './createRule'
 export * from './isResultType'
 export * from './literals'
+export * from './getOperatorPrecedence'
 
 // this is done for convenience - saves migrating all of the old rules
 export * from '@typescript-eslint/type-utils'
